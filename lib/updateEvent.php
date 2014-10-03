c60bf73030cc77e8a4c06650e6149cfed0d75f2d
Theodoros Liokos <tliokos@users.noreply.github.com>
2014-10-03 14:32:24 +0300

    Update updateEvent.php

--- lib/updateEvent.php
+++ lib/updateEvent.php
@@ -4,8 +4,6 @@
 	
 	require_once('connect.php');
 
-	// Fetch all events
-	
 	$sth = $dbh->prepare("UPDATE events SET title = ?, description = ? WHERE id = ?");
 		
 	$sth->execute(array($_POST['title'], $_POST['description'], $_POST['id']));
