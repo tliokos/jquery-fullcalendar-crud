5c133cbaba2ab4a90f9151cae25a3222c0cf9ea8
Theodoros Liokos <tliokos@gmail.com>
2014-10-05 03:27:57 +0300

    Bootstrap Components Added

--- sql/calendar.sql
+++ sql/calendar.sql
@@ -6,16 +6,13 @@
 -- Generation Time: Oct 04, 2014 at 11:56 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `calendar`
 --
